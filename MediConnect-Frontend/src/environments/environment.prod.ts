55e257d3f591271a30d2d9a4fc3f4065179454dc
mirfaizanali <mir_faizan_ali@outlook.com>
2026-03-15 03:55:21 +0530

    update prod api url

--- MediConnect-Frontend/src/environments/environment.prod.ts
+++ MediConnect-Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://mediconnectcomplete-production.up.railway.app/api'
+  apiUrl: 'https://mediconnectcomplete-production.up.railway.app'
 };
